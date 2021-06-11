642978b1f192ee014dcc38ea41a1c9d15016c1bd
kendzi <kendzi@posteo.de>
2021-06-11 14:14:20 +0200

    Add wrapper for Aggregate

--- bindings/python/wrapper.py
+++ bindings/python/wrapper.py
@@ -4,6 +4,7 @@ import blst
 
 DST_SIG = b"BLS_SIG_BLS12381G2_XMD:SHA-256_SSWU_RO_POP_"
 DST_POP = b"BLS_POP_BLS12381G2_XMD:SHA-256_SSWU_RO_POP_"
+INVALID_AGG_SIG = [False, bytes()]
 
 # sk_bytes, msg and dst must be of type bytes
 def sign(sk_bytes, msg, dst=DST_SIG):
@@ -65,3 +66,23 @@ def PopVerify(pk_bytes, proof):
     if not KeyValidate(pk_bytes):
         return False
     return verify(pk_bytes, pk_bytes, proof, DST_POP)
+
+
+# sigs must be an array of signatures where each signature is of type bytes
+# returns [True/False, aggregate signature]
+def Aggregate(sigs):
+    if len(sigs) == 0:
+        return INVALID_AGG_SIG
+    try:
+        agg_sig = blst.P2(sigs[0])
+    except:
+        # sigs[0] does not represent a point on E2
+        return INVALID_AGG_SIG
+    for sig in sigs[1:]:
+        try:
+            SIG = blst.P2_Affine(sig)
+        except:
+            # sig does not represent a point on E2
+            return INVALID_AGG_SIG
+        agg_sig.add(SIG)
+    return [True, agg_sig.compress()]
